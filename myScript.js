724681680a34db3816d2f76fe944e5436ff3fba5
Mohammed Saayim <mdsaayim@gmail.com>
2020-12-08 20:08:32 +0530

    input output processing

--- myScript.js
+++ myScript.js
@@ -1,2 +1,12 @@
-var userName= prompt("May I have your name?");
-alert("hello "+userName);
+var btnTranslate= document.querySelector("#btn-translate");
+var inputDiv= document.querySelector("#englishInput")
+var outputDiv= document.querySelector('#bananaOutput');
+function addEventHandler(){
+outputDiv.innerHTML=("adsfjkl "+inputDiv.value)
+};
+
+btnTranslate.addEventListener("click",  addEventHandler)
+
+var textArea= document.querySelector("textArea");
+
+
