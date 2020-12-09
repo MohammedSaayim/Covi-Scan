2ebfdf58a4b27e78060ca0b79fdb68daeedca4af
Mohammed Saayim <mdsaayim@gmail.com>
2020-12-09 22:39:48 +0530

    input output processing done

--- myScript.js
+++ myScript.js
@@ -1,12 +1,42 @@
 var btnTranslate= document.querySelector("#btn-translate");
-var inputDiv= document.querySelector("#englishInput")
-var outputDiv= document.querySelector('#bananaOutput');
-function addEventHandler(){
-outputDiv.innerHTML=("adsfjkl "+inputDiv.value)
-};
+var input= document.querySelector("#englishInput")
+var output= document.querySelector('#bananaOutput')
+console.log(input.value)
+var url="https://api.funtranslations.com/translate/minion.json?text="
+
+function constructUrl(inputTxt){
+    url=url+inputTxt
+    console.log(inputTxt)
+    return url
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with server! Please try again")
+}
+
+
+
+fetch(constructUrl(input.value))
+.then(function responseHandler(response){
+    return response.json()
+})
+
+.then(function logJson(json){
+    console.log(json.contents.translated)
+    var outputTxt=json.contents.translated
+    return outputTxt
+})
+
+.then(
+function addEventHandler(outputTxt){
+    output.innerText=outputTxt
+    console.log(innerText)
+    })
 
 btnTranslate.addEventListener("click",  addEventHandler)
 
 var textArea= document.querySelector("textArea");
+
 
 
